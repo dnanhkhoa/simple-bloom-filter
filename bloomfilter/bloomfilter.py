a8b8a8a0fd45fd32ef58be6c696d0ee1d4b69554
Anh Khoa <dnanhkhoa@live.com>
2018-08-11 00:02:09 +0700

    Add Scalable Bloom Filter class

--- bloomfilter/bloomfilter.py
+++ bloomfilter/bloomfilter.py
@@ -2,67 +2,193 @@
 # -*- coding: utf-8 -*-
 import math
 import mmh3
+from enum import Enum
+from struct import pack, unpack, calcsize
 
 from bitarray import bitarray
 
 
 class BloomFilter(object):
-    def __init__(self, max_items, fp_prob=1e-6):
-        self.__num_items = 0
-        self.__max_items = max_items
+    def __init__(self, size, fp_prob=1e-6):
+        self.__size_used = 0
+        self.__size = size
         self.__fp_prob = fp_prob
 
-        if self.__max_items:
-            self.__size = math.ceil(
-                -self.__max_items * math.log(self.__fp_prob) / math.log(2) ** 2
+        if self.__size:
+            self.__filter_size = math.ceil(
+                -self.__size * math.log(self.__fp_prob) / math.log(2) ** 2
             )
-            self.__num_hashes = round(self.__size * math.log(2) / self.__max_items)
+            self.__num_hashes = round(self.__filter_size * math.log(2) / self.__size)
 
-            self.__array = bitarray(self.__size, endian='little')
-            self.__array.setall(False)
+            self.__filter = bitarray(self.__filter_size, endian="little")
+            self.__filter.setall(False)
         else:
-            self.__array = bitarray(endian='little')
+            self.__filter = bitarray(endian="little")
 
     @classmethod
-    def load(cls, fp):
-        bloom_filter = cls()
-        # bf.__array = bitarray()
-        # bf.__array.fromfile(fp)
-        pass
+    def load(cls, fp, n=-1):
+        header = "<dQQQQ"
+        header_size = calcsize(header)
+        fp_prob, size_used, size, filter_size, num_hashes = unpack(
+            header, fp.read(header_size)
+        )
+        bloom_filter = cls(size=None, fp_prob=fp_prob)
+        if n >= header_size:
+            bloom_filter.__filter.frombytes(fp.read(n - header_size))
+        else:
+            bloom_filter.__filter.frombytes(fp.read())
+        assert (
+                bloom_filter.__filter.length() == filter_size
+        ), "Bloom filter size mismatch!"
+        bloom_filter.__size_used = size_used
+        bloom_filter.__size = size
+        bloom_filter.__filter_size = filter_size
+        bloom_filter.__num_hashes = num_hashes
+        return bloom_filter
 
     def save(self, fp):
-        pass
+        fp.write(
+            pack(
+                "<dQQQQ",
+                self.__fp_prob,
+                self.__size_used,
+                self.__size,
+                self.__filter_size,
+                self.__num_hashes,
+            )
+        )
+        fp.write(self.__filter.tobytes())
 
     @property
-    def size(self):
-        return self.__size
+    def filter_size(self):
+        return self.__filter_size
 
     @property
     def num_hashes(self):
         return self.__num_hashes
 
     @property
-    def max_items(self):
-        return self.__max_items
+    def fp_prob(self):
+        return self.__fp_prob
+
+    @property
+    def size(self):
+        return self.__size
 
     def __len__(self):
-        return self.__num_items
+        return self.__size_used
 
     def add(self, item):
-        if item not in self:
+        if item not in self and self.__size_used < self.__size:
             for i in range(self.__num_hashes):
-                self.__array[mmh3.hash(item, i) % self.__size] = True
-            self.__num_items += 1
+                self.__filter[mmh3.hash(item, i) % self.__filter_size] = True
+            self.__size_used += 1
 
     def __contains__(self, item):
         for i in range(self.__num_hashes):
-            if not self.__array[mmh3.hash(item, i) % self.__size]:
+            if not self.__filter[mmh3.hash(item, i) % self.__filter_size]:
                 return False
         return True
 
 
+class SizeGrowthRate(Enum):
+    SMALL = 2
+    LARGE = 4
+
+
+class ScalableBloomFilter(object):
+    """
+    References:
+    + http://gsd.di.uminho.pt/members/cbm/ps/dbloom.pdf
+    + https://github.com/jaybaird/python-bloomfilter
+    """
+
+    def __init__(
+            self,
+            initial_size=100,
+            initial_fp_prob=1e-6,
+            size_growth_rate=SizeGrowthRate.LARGE,
+            fp_prob_rate=0.9,
+    ):
+        self.__initial_size = initial_size
+        self.__initial_fp_prob = initial_fp_prob
+        self.__size_growth_rate = size_growth_rate
+        self.__fp_prob_rate = fp_prob_rate
+        self.__filters = []
+
+        if self.__initial_size:
+            self.__filters.append(
+                BloomFilter(size=self.__initial_size, fp_prob=self.__initial_fp_prob)
+            )
+
+    @classmethod
+    def load(cls, fp):
+        header = "<QdQdQ"
+        initial_size, initial_fp_prob, size_growth_rate, fp_prob_rate, num_filters = unpack(
+            header, fp.read(calcsize(header))
+        )
+        scalable_bloom_filter = cls(
+            initial_size=0,
+            initial_fp_prob=initial_fp_prob,
+            size_growth_rate=size_growth_rate,
+            fp_prob_rate=fp_prob_rate,
+        )
+        header = "<" + "Q" * num_filters
+        filter_sizes = unpack(header, fp.read(calcsize(header)))
+        for filter_size in filter_sizes:
+            scalable_bloom_filter.__filters.append(BloomFilter.load(fp, filter_size))
+
+    def save(self, fp):
+        fp.write(
+            pack(
+                "<QdQdQ",
+                self.__initial_size,
+                self.__initial_fp_prob,
+                self.__size_growth_rate,
+                self.__fp_prob_rate,
+                len(self.__filters),
+            )
+        )
+        header_pos = fp.tell()
+        header = "<" + "Q" * len(self.__filters)
+        fp.write("\0" * calcsize(header))
+        filter_sizes = []
+        for f in self.__filters:
+            begin = fp.tell()
+            f.save(fp)
+            filter_sizes.append(fp.tell() - begin)
+
+        fp.seek(header_pos)
+        fp.write(pack(header_pos, *filter_sizes))
+
+    @property
+    def size(self):
+        return sum(f.size for f in self.__filters)
+
+    def __len__(self):
+        return sum(len(f) for f in self.__filters)
+
+    def add(self, item):
+        if item not in self:
+            bloom_filter = self.__filters[-1]
+            if len(bloom_filter) >= bloom_filter.size:
+                bloom_filter = BloomFilter(
+                    size=bloom_filter.size * self.__size_growth_rate,
+                    fp_prob=bloom_filter.fp_prob * self.__fp_prob_rate,
+                )
+                self.__filters.append(bloom_filter)
+            bloom_filter.add(item)
+
+    def __contains__(self, item):
+        for f in reversed(self.__filters):
+            if item in f:
+                return True
+        return False
+
+
 def main():
     bf = BloomFilter(10)
+    bf.add("Khoa")
     pass
 
 
